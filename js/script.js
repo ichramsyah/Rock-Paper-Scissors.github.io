aa3e649452c7b8b0dc573cc86ce35786bc5f481e
ichramsyah <iamabdulrahman15@gmail.com>
2025-02-13 22:50:04 +0700

    u

--- js/script.js
+++ js/script.js
@@ -1,45 +1,55 @@
 
-document.addEventListener("DOMContentLoaded", function(){
-    let heroImage = document.querySelector(".hero");
+ document.addEventListener("DOMContentLoaded", function(){
+     let heroImage = document.querySelector(".hero");
 
-    setTimeout(() => {
-        heroImage.style.opacity = "1";
-    }, 1500);
+     setTimeout(() => {
+         heroImage.style.opacity = "1";
+     }, 1500);
     
-    setTimeout(() => {
-        heroImage.style.opacity = "0";
-    }, 6000);
+     setTimeout(() => {
+         heroImage.style.opacity = "0";
+     }, 4000);
     
-    setTimeout(() => {
-        heroImage.remove();
-    }, 7000);
+     setTimeout(() => {
+         heroImage.remove();
+     }, 4500);
     
-})
+ })
 
-document.addEventListener("DOMContentLoaded", function(){
-    let computer = document.querySelector('.computer');
-    
-    setTimeout(() => {
-        computer.style.opacity = '1';
-        computer.style.transform = "translateY(0)";
-    }, 8000);
-})
+ document.addEventListener("DOMContentLoaded", function(){
+     let computer = document.querySelector('.computer');  
+     setTimeout(() => {
+         computer.style.display = 'flex';
+     }, 4700);
 
+     setTimeout(() => {
+         computer.style.opacity = '1';
+         computer.style.transform = "translateY(0)";
+     }, 5000);
+ })
+ 
+  document.addEventListener("DOMContentLoaded", function(){
+      let info = document.querySelector('.info');
+ 
+      setTimeout(() => {
+          info.style.display = 'block';
+      }, 4900);
 
-document.addEventListener("DOMContentLoaded", function (){
-    let card = document.querySelector('.area-player');
+      setTimeout(() => {
+          info.style.opacity = '1';
+          info.style.transform = 'translateY(0)';
+      }, 5300);
+  })
 
-    setTimeout(() => {
-        card.style.opacity = '1';
-        card.style.transform = 'translateY(0)';
-    }, 8500);
-})
+ document.addEventListener("DOMContentLoaded", function (){
+     let card = document.querySelector('.area-player');
 
-document.addEventListener("DOMContentLoaded", function(){
-    let info = document.querySelector('.info');
+     setTimeout(() => {
+         card.style.display = 'flex';
+     }, 4900);
 
-    setTimeout(() => {
-        info.style.opacity = '1';
-        info.style.transform = 'translateY(0)';
-    }, 8300);
-})
+     setTimeout(() => {
+         card.style.opacity = '1';
+         card.style.transform = 'translateY(0)';
+     }, 5500);
+ })
